0b14c540b5d4d3575a160e1ba89f7bbcd8ce9f58
lengushuru charles <charleslengushuru@gmail.com>
2023-02-25 16:19:08 +0300

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,96 @@
 import './styles.css';
 
-const list = document.querySelector('#list');
+const taskInput = document.querySelector('.addtolist');
+const taskBox = document.querySelector('.task-box');
 
-const tasks = [
-  {
-    description: 'wash car',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'call mum',
-    completed: false,
-    index: 3,
-  },
+const completed = false;
+let index = 0;
+let tasksContainer = [];
 
-];
+function showTodo() {
+  let list = '';
+  tasksContainer.forEach((element) => {
+    list += `<li class="task">
+                        <input type="checkbox" class="checkme">
+                        <input type="text" value="${element.description}" class="tasks" id="item${element.index}" readonly>
 
-tasks.sort((a, b) => a.index - b.index);
+                        <i class="fa-solid uil-ellipsis-v edit " id="edit${element.index}" onclick="editItem(${element.index});"></i>
+                        <i class="fa-solid fa-trash delete" id="delete${element.index}" onclick="removeItem(${element.index});"></i>
+        
+                        </li>`;
+  });
 
-tasks.forEach((element) => {
-  const container = document.createElement('div');
-  container.className = 'activity';
-  container.innerHTML = `
-  <div class = 'activity1'>
-    <input type = "checkbox" >
-    <p>${element.description}</p>
-  </div>
-  <i class="uil uil-ellipsis-v"></i>`;
+  taskBox.innerHTML = list || '<p class="no-item">You don\'t have any task here</p>';
+  taskInput.value = '';
+}
 
-  list.appendChild(container);
+taskInput.addEventListener('keypress', (keys) => {
+  if (keys.key === 'Enter' && taskInput.value !== '') {
+    const storedList = localStorage.getItem('todo-list');
+
+    if (storedList === null) {
+      tasksContainer = [];
+    } else {
+      tasksContainer = JSON.parse(storedList);
+      index = tasksContainer.length === 0 ? 0 : tasksContainer.length;
+    }
+
+    const LocalStore = {
+      index,
+      description: taskInput.value,
+      completed,
+    };
+    tasksContainer.push(LocalStore);
+    localStorage.setItem('todo-list', JSON.stringify(tasksContainer));
+    showTodo();
+  }
 });
+
+window.onload = () => {
+  if (localStorage.getItem('todo-list')) {
+    tasksContainer = JSON.parse(localStorage.getItem('todo-list'));
+  }
+  showTodo();
+};
+
+window.editItem = (index) => {
+  const editBtn = document.getElementById(`edit${index}`);
+  const deletebtn = document.getElementById(`delete${index}`);
+
+  deletebtn.style.display = 'block';
+  editBtn.style.display = 'none';
+  const mainItem = document.getElementById(`item${index}`);
+  mainItem.removeAttribute('readonly');
+  const { length } = mainItem.value;
+  mainItem.setSelectionRange(length, length);
+};
+
+window.removeItem = (index) => {
+  const editBtn = document.getElementById(`edit${index}`);
+  const deletebtn = document.getElementById(`delete${index}`);
+
+  deletebtn.style.display = 'none';
+  editBtn.style.display = 'block';
+  const storedData = localStorage.getItem('todo-list');
+  tasksContainer = JSON.parse(storedData);
+  tasksContainer.splice(index, 1);
+  for (let i = 0; i < tasksContainer.length; i += 1) {
+    tasksContainer[i].index = i;
+  }
+  localStorage.setItem('todo-list', JSON.stringify(tasksContainer));
+  showTodo();
+};
+
+const saveTaskOnBlur = (event) => {
+  const taskItem = event.target;
+  const itemId = taskItem.getAttribute('id');
+  const index = parseInt(itemId.replace('item', ''), 10);// radi
+  tasksContainer[index].description = taskItem.value;
+  localStorage.setItem('todo-list', JSON.stringify(tasksContainer));
+  const editBtn = document.getElementById(`edit${index}`);
+  const deletebtn = document.getElementById(`delete${index}`);
+
+  deletebtn.style.display = 'none';
+  editBtn.style.display = 'block';
+};
+taskBox.addEventListener('blur', saveTaskOnBlur, true);
